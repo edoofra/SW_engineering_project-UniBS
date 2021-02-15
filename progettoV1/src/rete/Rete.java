8091c91271c6657557048252dbaf8d97c6252558
edoofra <edoardo.fra.ef@gmail.com>
2021-02-15 17:59:02 +0100

    Update Rete.java

--- progettoV1/src/rete/Rete.java
+++ progettoV1/src/rete/Rete.java
@@ -1,10 +1,11 @@
 package rete;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Rete {
 	
-	ArrayList<Posto> insiemePosti = new ArrayList<Posto>();
-	ArrayList<Transizione> insiemeTransizioni = new ArrayList<Transizione>();
-	ArrayList<ElemFlusso> relazioneFlusso = new ArrayList<ElemFlusso>();	
+	HashSet<Posto> insiemePosti = new HashSet<Posto>();
+	HashSet<Transizione> insiemeTransizioni = new HashSet<Transizione>();
+	HashSet<ElemFlusso> relazioneFlusso = new HashSet<ElemFlusso>();	
 }
