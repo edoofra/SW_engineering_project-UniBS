2a2335634b4e1fc2b401d63bcacb7f8a97a711b4
edoofra <edoardo.fra.ef@gmail.com>
2021-02-15 19:00:32 +0100

    Update Rete.java

--- progettoV1/src/rete/Rete.java
+++ progettoV1/src/rete/Rete.java
@@ -1,11 +1,15 @@
 package rete;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Rete {
 	
 	HashSet<Posto> insiemePosti = new HashSet<Posto>();
 	HashSet<Transizione> insiemeTransizioni = new HashSet<Transizione>();
-	HashSet<ElemFlusso> relazioneFlusso = new HashSet<ElemFlusso>();	
+	HashSet<ElemFlusso> relazioneFlusso = new HashSet<ElemFlusso>();
+	
+	public boolean emptyControl() {
+		if (insiemePosti.isEmpty() || insiemeTransizioni.isEmpty()) return true;
+		else return false;
+	}
 }
