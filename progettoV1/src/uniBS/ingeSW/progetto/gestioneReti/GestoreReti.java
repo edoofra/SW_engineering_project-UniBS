2b17fb9cca66c91ccd79f7766dd8458f86456280
edoofra <edoardo.fra.ef@gmail.com>
2021-02-20 11:45:13 +0100

    Update GestoreReti.java

--- progettoV1/src/uniBS/ingeSW/progetto/gestioneReti/GestoreReti.java
+++ progettoV1/src/uniBS/ingeSW/progetto/gestioneReti/GestoreReti.java
@@ -1,24 +1,26 @@
 package uniBS.ingeSW.progetto.gestioneReti;
 
-import java.util.HashSet;
+import java.util.HashMap;
 import uniBS.ingeSW.progetto.rete.Rete;
 
 //classe per la gestione di tutte le reti salvate dal configuratore
 public class GestoreReti {
 
-    HashSet<Rete> listaRetiConfiguratore;
+    HashMap<String, Rete> listaRetiConfiguratore;
 
     public GestoreReti() {
 
-	this.listaRetiConfiguratore = new HashSet<Rete>();
+	this.listaRetiConfiguratore = new HashMap<String, Rete>();
     }
 
-    public boolean addRete(Rete toAdd) {
-
-	if (listaRetiConfiguratore.contains(toAdd))
-	    return false;
-	listaRetiConfiguratore.add(toAdd);
-	return true;
+    public int addRete(String name, Rete toAdd) {
+	
+	if (listaRetiConfiguratore.containsKey(name))
+	    return 1;
+	if (listaRetiConfiguratore.containsValue(toAdd))
+	    return 2;
+	listaRetiConfiguratore.put(name, toAdd);
+	return 0;
     }
 
 }
