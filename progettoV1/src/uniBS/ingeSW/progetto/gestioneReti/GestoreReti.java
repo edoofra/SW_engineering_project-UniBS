8133c1fd968ccf1bbcd81384d3119924b3ab0c94
edoofra <edoardo.fra.ef@gmail.com>
2021-03-05 17:25:37 +0100

    formatting

--- progettoV1/src/uniBS/ingeSW/progetto/gestioneReti/GestoreReti.java
+++ progettoV1/src/uniBS/ingeSW/progetto/gestioneReti/GestoreReti.java
@@ -7,58 +7,59 @@ import uniBS.ingeSW.progetto.rete.Rete;
 //classe per la gestione di tutte le reti salvate dal configuratore
 public class GestoreReti {
 
-    HashMap<String, Rete> listaRetiConfiguratore;
+	HashMap<String, Rete> listaRetiConfiguratore;
 
-    //costruttore
-    public GestoreReti() {
-	this.listaRetiConfiguratore = new HashMap<String, Rete>();
-    }
+	// costruttore
+	public GestoreReti() {
+		this.listaRetiConfiguratore = new HashMap<String, Rete>();
+	}
 
-    public HashMap<String, Rete> getListaRetiConfiguratore() {
-	return listaRetiConfiguratore;
-    }
-    
-    //ritorna la lista delle chiavi come Array ( piu' comodo da gestire )
-    public String[] getKeyLIst() {
-	return (String[]) listaRetiConfiguratore.keySet().toArray();
-    }
+	public HashMap<String, Rete> getListaRetiConfiguratore() {
+		return listaRetiConfiguratore;
+	}
 
-    //aggiunge una rete alla map con controlli
-    //ritorna codice errore 1 se esiste gia' nome
-    //ritorna codice errore 2 se esiste gia' rete
-    //gestione codice errori andra' fatta in interazione con utente
-    public int addRete(String name, Rete toAdd) {
-	if (listaRetiConfiguratore.containsKey(name))
-	    return 1;
-	if (listaRetiConfiguratore.containsValue(toAdd))
-	    return 2;
-	listaRetiConfiguratore.put(name, toAdd);
-	return 0;
-    }
-    
-    public boolean isEqual (Rete rete1, Rete rete2) {
-	boolean uguali = false;
-	
-	for (ElemFlusso elem1 : rete1.getRelazioneFlusso()) {
-	    for (ElemFlusso elem2 : rete2.getRelazioneFlusso()) {
-		if(elem1.equals(elem2)) {
-		    uguali = true;
-		    break;
+	// ritorna la lista delle chiavi come Array ( piu' comodo da gestire )
+	public String[] getKeyLIst() {
+		return (String[]) listaRetiConfiguratore.keySet().toArray();
+	}
+
+	// aggiunge una rete alla map con controlli
+	// ritorna codice errore 1 se esiste gia' nome
+	// ritorna codice errore 2 se esiste gia' rete
+	// gestione codice errori andra' fatta in interazione con utente
+	public int addRete(String name, Rete toAdd) {
+		if (listaRetiConfiguratore.containsKey(name))
+			return 1;
+		if (listaRetiConfiguratore.containsValue(toAdd))
+			return 2;
+		listaRetiConfiguratore.put(name, toAdd);
+		return 0;
+	}
+
+	public boolean isEqual(Rete rete1, Rete rete2) {
+		boolean uguali = false;
+
+		for (ElemFlusso elem1 : rete1.getRelazioneFlusso()) {
+			for (ElemFlusso elem2 : rete2.getRelazioneFlusso()) {
+				if (elem1.equals(elem2)) {
+					uguali = true;
+					break;
+				}
+			}
+			if (!uguali)
+				return false;
 		}
-	    }
-	    if(!uguali) return false;
+		return true;
 	}
-	return true;
-    }
 
-    public String toString() {
-	StringBuilder StringList = new StringBuilder();
-	String[] keyList = getKeyLIst();
-	for (int i = 0; i < keyList.length; i++) {
-	    StringList.append(keyList[i]);
+	public String toString() {
+		StringBuilder StringList = new StringBuilder();
+		String[] keyList = getKeyLIst();
+		for (int i = 0; i < keyList.length; i++) {
+			StringList.append(keyList[i]);
+		}
+		return StringList.toString();
+
 	}
-	return StringList.toString();
-
-    }
 
 }
--- progettoV1/src/uniBS/ingeSW/progetto/utils/InterazioneUtente.java
+++ progettoV1/src/uniBS/ingeSW/progetto/utils/InterazioneUtente.java
@@ -8,259 +8,215 @@ import uniBS.ingeSW.progetto.rete.Rete;
 import uniBS.ingeSW.progetto.rete.Transizione;
 
 public class InterazioneUtente {
-    private static Scanner lettore = creaScanner();
-	  
-	  private final static String ERRORE_FORMATO = "Attenzione: il dato inserito non e' nel formato corretto";
-	  private final static String ERRORE_MINIMO= "Attenzione: e' richiesto un valore maggiore o uguale a ";
-	  private final static String ERRORE_STRINGA_VUOTA= "Attenzione: non hai inserito alcun carattere";
-	  private final static String ERRORE_MASSIMO= "Attenzione: e' richiesto un valore minore o uguale a ";
-	  private final static String MESSAGGIO_AMMISSIBILI= "Attenzione: i caratteri ammissibili sono: ";
-	  private final static char RISPOSTA_SI='S';
-	  private final static char RISPOSTA_NO='N';
+	private static Scanner lettore = creaScanner();
 
-	  
+	private final static String ERRORE_FORMATO = "Attenzione: il dato inserito non e' nel formato corretto";
+	private final static String ERRORE_MINIMO = "Attenzione: e' richiesto un valore maggiore o uguale a ";
+	private final static String ERRORE_STRINGA_VUOTA = "Attenzione: non hai inserito alcun carattere";
+	private final static String ERRORE_MASSIMO = "Attenzione: e' richiesto un valore minore o uguale a ";
+	private final static String MESSAGGIO_AMMISSIBILI = "Attenzione: i caratteri ammissibili sono: ";
+	private final static char RISPOSTA_SI = 'S';
+	private final static char RISPOSTA_NO = 'N';
 
-	  private static Scanner creaScanner ()
-	  {
-	   Scanner creato = new Scanner(System.in);
-	   creato.useDelimiter(System.getProperty("line.separator"));
-	   return creato;
-	  }
-	  
-	  public static String leggiStringa (String messaggio)
-	  {
-		  System.out.print(messaggio);
-		  return lettore.next();
-	  }
-	  
-	  public static String leggiStringaNonVuota(String messaggio)
-	  {
-	   boolean finito=false;
-	   String lettura = null;
-	   do
-	   {
-		 lettura = leggiStringa(messaggio);
-		 lettura = lettura.trim();
-		 if (lettura.length() > 0)
-		  finito=true;
-		 else
-		  System.out.println(ERRORE_STRINGA_VUOTA);
-	   } while (!finito);
-	   
-	   return lettura;
-	  }
-	  
-	  public static char leggiChar (String messaggio)
-	  {
-	   boolean finito = false;
-	   char valoreLetto = '\0';
-	   do
-	    {
-	     System.out.print(messaggio);
-	     String lettura = lettore.next();
-	     if (lettura.length() > 0)
-	      {
-	       valoreLetto = lettura.charAt(0);
-	       finito = true;
-	      }
-	     else
-	      {
-	       System.out.println(ERRORE_STRINGA_VUOTA);
-	      }
-	    } while (!finito);
-	   return valoreLetto;
-	  }
-	  
-	  public static char leggiUpperChar (String messaggio, String ammissibili)
-	  {
-	   boolean finito = false;
-	   char valoreLetto = '\0';
-	   do
-	   {
-	    valoreLetto = leggiChar(messaggio);
-	    valoreLetto = Character.toUpperCase(valoreLetto);
-	    if (ammissibili.indexOf(valoreLetto) != -1)
-		 finito  = true;
-	    else
-	     System.out.println(MESSAGGIO_AMMISSIBILI + ammissibili);
-	   } while (!finito);
-	   return valoreLetto;
-	  }
-	  
-	  
-	  public static int leggiIntero (String messaggio)
-	  {
-	   boolean finito = false;
-	   int valoreLetto = 0;
-	   do
-	    {
-	     System.out.print(messaggio);
-	     try
-	      {
-	       valoreLetto = lettore.nextInt();
-	       finito = true;
-	      }
-	     catch (InputMismatchException e)
-	      {
-	       System.out.println(ERRORE_FORMATO);
-	       String daButtare = lettore.next();
-	      }
-	    } while (!finito);
-	   return valoreLetto;
-	  }
+	private static Scanner creaScanner() {
+		Scanner creato = new Scanner(System.in);
+		creato.useDelimiter(System.getProperty("line.separator"));
+		return creato;
+	}
 
-	  public static int leggiInteroPositivo(String messaggio)
-	  {
-		  return leggiInteroConMinimo(messaggio,1);
-	  }
-	  
-	  public static int leggiInteroNonNegativo(String messaggio)
-	  {
-		  return leggiInteroConMinimo(messaggio,0);
-	  }
-	  
-	  
-	  public static int leggiInteroConMinimo(String messaggio, int minimo)
-	  {
-	   boolean finito = false;
-	   int valoreLetto = 0;
-	   do
-	    {
-	     valoreLetto = leggiIntero(messaggio);
-	     if (valoreLetto >= minimo)
-	      finito = true;
-	     else
-	      System.out.println(ERRORE_MINIMO + minimo);
-	    } while (!finito);
-	     
-	   return valoreLetto;
-	  }
+	public static String leggiStringa(String messaggio) {
+		System.out.print(messaggio);
+		return lettore.next();
+	}
 
-	  public static int leggiIntero(String messaggio, int minimo, int massimo)
-	  {
-	   boolean finito = false;
-	   int valoreLetto = 0;
-	   do
-	    {
-	     valoreLetto = leggiIntero(messaggio);
-	     if (valoreLetto >= minimo && valoreLetto<= massimo)
-	      finito = true;
-	     else
-	      if (valoreLetto < minimo)
-	         System.out.println(ERRORE_MINIMO + minimo);
-	      else
-	    	 System.out.println(ERRORE_MASSIMO + massimo); 
-	    } while (!finito);
-	     
-	   return valoreLetto;
-	  }
+	public static String leggiStringaNonVuota(String messaggio) {
+		boolean finito = false;
+		String lettura = null;
+		do {
+			lettura = leggiStringa(messaggio);
+			lettura = lettura.trim();
+			if (lettura.length() > 0)
+				finito = true;
+			else
+				System.out.println(ERRORE_STRINGA_VUOTA);
+		} while (!finito);
 
-	  
-	  public static double leggiDouble (String messaggio)
-	  {
-	   boolean finito = false;
-	   double valoreLetto = 0;
-	   do
-	    {
-	     System.out.print(messaggio);
-	     try
-	      {
-	       valoreLetto = lettore.nextDouble();
-	       finito = true;
-	      }
-	     catch (InputMismatchException e)
-	      {
-	       System.out.println(ERRORE_FORMATO);
-	       String daButtare = lettore.next();
-	      }
-	    } while (!finito);
-	   return valoreLetto;
-	  }
-	 
-	  public static double leggiDoubleConMinimo (String messaggio, double minimo)
-	  {
-	   boolean finito = false;
-	   double valoreLetto = 0;
-	   do
-	    {
-	     valoreLetto = leggiDouble(messaggio);
-	     if (valoreLetto >= minimo)
-	      finito = true;
-	     else
-	      System.out.println(ERRORE_MINIMO + minimo);
-	    } while (!finito);
-	     
-	   return valoreLetto;
-	  }
+		return lettura;
+	}
 
-	  
-	  public static boolean yesOrNo(String messaggio)
-	  {
-		  String mioMessaggio = messaggio + "("+RISPOSTA_SI+"/"+RISPOSTA_NO+")";
-		  char valoreLetto = leggiUpperChar(mioMessaggio,String.valueOf(RISPOSTA_SI)+String.valueOf(RISPOSTA_NO));
-		  
-		  if (valoreLetto == RISPOSTA_SI)
+	public static char leggiChar(String messaggio) {
+		boolean finito = false;
+		char valoreLetto = '\0';
+		do {
+			System.out.print(messaggio);
+			String lettura = lettore.next();
+			if (lettura.length() > 0) {
+				valoreLetto = lettura.charAt(0);
+				finito = true;
+			} else {
+				System.out.println(ERRORE_STRINGA_VUOTA);
+			}
+		} while (!finito);
+		return valoreLetto;
+	}
+
+	public static char leggiUpperChar(String messaggio, String ammissibili) {
+		boolean finito = false;
+		char valoreLetto = '\0';
+		do {
+			valoreLetto = leggiChar(messaggio);
+			valoreLetto = Character.toUpperCase(valoreLetto);
+			if (ammissibili.indexOf(valoreLetto) != -1)
+				finito = true;
+			else
+				System.out.println(MESSAGGIO_AMMISSIBILI + ammissibili);
+		} while (!finito);
+		return valoreLetto;
+	}
+
+	public static int leggiIntero(String messaggio) {
+		boolean finito = false;
+		int valoreLetto = 0;
+		do {
+			System.out.print(messaggio);
+			try {
+				valoreLetto = lettore.nextInt();
+				finito = true;
+			} catch (InputMismatchException e) {
+				System.out.println(ERRORE_FORMATO);
+				String daButtare = lettore.next();
+			}
+		} while (!finito);
+		return valoreLetto;
+	}
+
+	public static int leggiInteroPositivo(String messaggio) {
+		return leggiInteroConMinimo(messaggio, 1);
+	}
+
+	public static int leggiInteroNonNegativo(String messaggio) {
+		return leggiInteroConMinimo(messaggio, 0);
+	}
+
+	public static int leggiInteroConMinimo(String messaggio, int minimo) {
+		boolean finito = false;
+		int valoreLetto = 0;
+		do {
+			valoreLetto = leggiIntero(messaggio);
+			if (valoreLetto >= minimo)
+				finito = true;
+			else
+				System.out.println(ERRORE_MINIMO + minimo);
+		} while (!finito);
+
+		return valoreLetto;
+	}
+
+	public static int leggiIntero(String messaggio, int minimo, int massimo) {
+		boolean finito = false;
+		int valoreLetto = 0;
+		do {
+			valoreLetto = leggiIntero(messaggio);
+			if (valoreLetto >= minimo && valoreLetto <= massimo)
+				finito = true;
+			else if (valoreLetto < minimo)
+				System.out.println(ERRORE_MINIMO + minimo);
+			else
+				System.out.println(ERRORE_MASSIMO + massimo);
+		} while (!finito);
+
+		return valoreLetto;
+	}
+
+	public static double leggiDouble(String messaggio) {
+		boolean finito = false;
+		double valoreLetto = 0;
+		do {
+			System.out.print(messaggio);
+			try {
+				valoreLetto = lettore.nextDouble();
+				finito = true;
+			} catch (InputMismatchException e) {
+				System.out.println(ERRORE_FORMATO);
+				String daButtare = lettore.next();
+			}
+		} while (!finito);
+		return valoreLetto;
+	}
+
+	public static double leggiDoubleConMinimo(String messaggio, double minimo) {
+		boolean finito = false;
+		double valoreLetto = 0;
+		do {
+			valoreLetto = leggiDouble(messaggio);
+			if (valoreLetto >= minimo)
+				finito = true;
+			else
+				System.out.println(ERRORE_MINIMO + minimo);
+		} while (!finito);
+
+		return valoreLetto;
+	}
+
+	public static boolean yesOrNo(String messaggio) {
+		String mioMessaggio = messaggio + "(" + RISPOSTA_SI + "/" + RISPOSTA_NO + ")";
+		char valoreLetto = leggiUpperChar(mioMessaggio, String.valueOf(RISPOSTA_SI) + String.valueOf(RISPOSTA_NO));
+
+		if (valoreLetto == RISPOSTA_SI)
 			return true;
-		  else
+		else
 			return false;
-	  }
-	  
-	 
-	 public static void creazioneRete(Rete daCreare) {
-	     System.out.println("hai deciso di creare una rete");
-	     aggiuntaPosto(daCreare);
-	     aggiuntaTransizione(daCreare);
-	     aggiuntaElementoFlusso(daCreare);
-	     
-	     
-	 }
+	}
+
+	public static void creazioneRete(Rete daCreare) {
+		System.out.println("hai deciso di creare una rete");
+		aggiuntaPosto(daCreare);
+		aggiuntaTransizione(daCreare);
+		aggiuntaElementoFlusso(daCreare);
+
+	}
 
 	private static void aggiuntaPosto(Rete daCreare) {
-	    boolean risposta = true;
-	     System.out.println("per prima cosa devi aggiungere i posti.");
-	     while (risposta != false) {
-		 
-		 String nome = leggiStringaNonVuota("scegli un nome per il posto");
-		 Posto nuovo = new Posto(nome);
-		 daCreare.addPosto(nuovo);
-		 risposta = yesOrNo("vuoi aggiungere altri posti?");
-	     }
-	}
-	
-	private static void aggiuntaTransizione(Rete daCreare) {
-	    boolean risposta = true;
-	     System.out.println("ora devi aggiungere le transizioni");
-	     while (risposta != false) {
-		 
-		 String nome = leggiStringaNonVuota("scegli un nome per la transizione");
-		 Transizione nuovo = new Transizione(nome);
-		 daCreare.addTrans(nuovo);
-		 risposta = yesOrNo("vuoi aggiungere altre transizioni?");
-	     }
-	}
-	
-	private static void aggiuntaElementoFlusso(Rete daCreare) {
-	    boolean risposta = true;
-	     System.out.println("ora devi aggiungere gli elementi di flusso");
-	     while (risposta != false) {
-		 
-		 int scelta = leggiIntero("vuoi partire da un posto (1) o da una transizione(2)?", 1, 2);
-		 if(scelta == 1) {
-		     System.out.println(daCreare.getInsiemePosti());
-		 }
-		 else System.out.println(daCreare.getInsiemeTransizioni());
-		 //chiamo metodo per cercare obj da stringa
-		 //printo altra lista
-		 //chiamo metodo x secondo obj
-		 //creo elem flusso
-		 //aggiungo elem flusso
-		 risposta = yesOrNo("vuoi aggiungere altri elementi di flusso?");
-	     }
-	}
-	
-	
-	  
-	  
+		boolean risposta = true;
+		System.out.println("per prima cosa devi aggiungere i posti.");
+		while (risposta != false) {
 
+			String nome = leggiStringaNonVuota("scegli un nome per il posto");
+			Posto nuovo = new Posto(nome);
+			daCreare.addPosto(nuovo);
+			risposta = yesOrNo("vuoi aggiungere altri posti?");
+		}
+	}
+
+	private static void aggiuntaTransizione(Rete daCreare) {
+		boolean risposta = true;
+		System.out.println("ora devi aggiungere le transizioni");
+		while (risposta != false) {
+
+			String nome = leggiStringaNonVuota("scegli un nome per la transizione");
+			Transizione nuovo = new Transizione(nome);
+			daCreare.addTrans(nuovo);
+			risposta = yesOrNo("vuoi aggiungere altre transizioni?");
+		}
+	}
+
+	private static void aggiuntaElementoFlusso(Rete daCreare) {
+		boolean risposta = true;
+		System.out.println("ora devi aggiungere gli elementi di flusso");
+		while (risposta != false) {
+
+			int scelta = leggiIntero("vuoi partire da un posto (1) o da una transizione(2)?", 1, 2);
+			if (scelta == 1) {
+				System.out.println(daCreare.getInsiemePosti());
+			} else
+				System.out.println(daCreare.getInsiemeTransizioni());
+			// chiamo metodo per cercare obj da stringa
+			// printo altra lista
+			// chiamo metodo x secondo obj
+			// creo elem flusso
+			// aggiungo elem flusso
+			risposta = yesOrNo("vuoi aggiungere altri elementi di flusso?");
+		}
+	}
 
 }
