66e822f8ae87bfabd4c7c0f4127d54ad0e002400
edoofra <edoardo.fra.ef@gmail.com>
2021-03-10 23:59:45 +0100

    javadoc

--- progettoV1/src/uniBS/ingeSW/progetto/rete/Posto.java
+++ progettoV1/src/uniBS/ingeSW/progetto/rete/Posto.java
@@ -1,11 +1,25 @@
 package uniBS.ingeSW.progetto.rete;
-
+/**
+ * @author Camilla Bonomini
+ * @author Edoardo Fratus
+ * @author Lorenzo Bargnani
+ * Classe per implementazione di un posto di una rete
+ * Eredita dalla classe ElementoSemplice
+ */
 public class Posto extends ElementoSemplice {
 	
+	/**
+	 * Costruttore
+	 * @param nome nome scelto dal configuratore al momento della creazione
+	 */
 	public Posto(String nome) {
 		this.nome = "P:" + nome;
 	}
 
+	/**
+	 * @return le proprietà principali che descrivono un posto in una rete
+	 */
+	@Override
 	public String getProperties(){
 		return this.nome;
 	}
