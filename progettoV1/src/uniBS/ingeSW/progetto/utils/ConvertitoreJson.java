a22834f6770383cba1f112775207f8717c4978f6
edoofra <edoardo.fra.ef@gmail.com>
2021-03-12 13:06:27 +0100

    Update ConvertitoreJson.java

--- progettoV1/src/uniBS/ingeSW/progetto/utils/ConvertitoreJson.java
+++ progettoV1/src/uniBS/ingeSW/progetto/utils/ConvertitoreJson.java
@@ -1,19 +1,13 @@
 package uniBS.ingeSW.progetto.utils;
 import com.google.gson.Gson;
 import uniBS.ingeSW.progetto.gestioneReti.GestoreReti;
-import uniBS.ingeSW.progetto.rete.ElementoSemplice;
 
 
 public class ConvertitoreJson {
     
-    public static String daOggettoAJson(Object oggetto){
+    public static String daOggettoAJson(GestoreReti oggetto){
         Gson convertitore = new Gson();
         return convertitore.toJson(oggetto);
-    }
-
-    public static ElementoSemplice daJsonAOggetto(String json){
-        Gson convertitore = new Gson();
-        return convertitore.fromJson(json, ElementoSemplice.class);
     }
 
     public static GestoreReti daJsonAOggettoHashSet(String json){
