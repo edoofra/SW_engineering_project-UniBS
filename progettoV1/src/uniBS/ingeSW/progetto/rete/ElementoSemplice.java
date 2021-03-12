c07dd7f89b0a4ca48dfbe09630b148a86465c154
edoofra <edoardo.fra.ef@gmail.com>
2021-03-12 13:11:06 +0100

    javadoc

--- progettoV1/src/uniBS/ingeSW/progetto/rete/ElementoSemplice.java
+++ progettoV1/src/uniBS/ingeSW/progetto/rete/ElementoSemplice.java
@@ -1,13 +1,26 @@
 package uniBS.ingeSW.progetto.rete;
 
+/**
+ * @author Lorenzo Bargnani
+ * @author Camilla Bonomini
+ * @author Edoardo Fratus
+ * Classe per la gestione ad un livello più alto di posti e transizioni
+ */
 public abstract class ElementoSemplice {
 	
+	/**
+	 * nome dell'elemento della rete
+	 */
 	String nome;
 
 	public String getName(){
 		return this.nome;
 	}
-	
+	/**
+	 * Metodo per ottenere le proprietà principali di ogni elemento 
+	 * Viene fatto override in ogni elemento che eredita da questa classe 
+	 * @return nome dell'elemento (attributo base di ogni elemento della rete)
+	 */
 	public String getProperties() {
 		return this.nome;
 	}
