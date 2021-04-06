7df387603272406f5ace602655dc239e31326f03
edoofra <edoardo.fra.ef@gmail.com>
2021-04-06 15:25:49 +0200

    Update ElemFlusso.java

--- progettoV1/src/uniBS/ingeSW/progetto/rete/ElemFlusso.java
+++ progettoV1/src/uniBS/ingeSW/progetto/rete/ElemFlusso.java
@@ -1,9 +1,16 @@
 package uniBS.ingeSW.progetto.rete;
 
+/**
+ * @author Lorenzo Bargnani
+ * @author Edoardo Fratus
+ * @author Camilla Bonomini
+ * classe per l'implementazione di un elemento di flusso di una rete
+ */
 public class ElemFlusso {
 
-	private ElementoSemplice elem1;
-	private ElementoSemplice elem2;
+	
+	private ElementoSemplice elem1; //elemento di partenza	
+	private ElementoSemplice elem2; //elemento di arrivo
 	
 	public ElemFlusso(ElementoSemplice elem1, ElementoSemplice elem2) {
 		this.elem1 = elem1;
@@ -18,10 +25,19 @@ public class ElemFlusso {
 		return elem2;
 	}
 	
+	/**
+	 * @return nome dell'elemento di flusso dato da nome partenza + nome arrivo
+	 */
 	public String getName() {
 	    return "( " + elem1.getProperties() + "," + elem2.getProperties() + " )";
 	}
 	
+	/**
+	 * confronta due elementi di flusso per valutare se siano uguali o meno
+	 * @param flusso1
+	 * @param flusso2
+	 * @return true se sono uguali, false altrimenti
+	 */
 	public boolean equal(ElemFlusso flusso1, ElemFlusso flusso2) {
 	    
 	    return ((flusso1.getElem1().equals(flusso2.getElem1()) && 
