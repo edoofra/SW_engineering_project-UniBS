57d59131ac977b0972896f84898f61185abe3989
edoofra <edoardo.fra.ef@gmail.com>
2021-04-06 15:10:50 +0200

    Update Transizione.java

--- progettoV1/src/uniBS/ingeSW/progetto/rete/Transizione.java
+++ progettoV1/src/uniBS/ingeSW/progetto/rete/Transizione.java
@@ -1,11 +1,24 @@
 package uniBS.ingeSW.progetto.rete;
-
+/**
+ * @author Edoardo Fratus
+ * @author Camilla Bonomini
+ * @author Lorenzo Bargnani
+ * Classe per l'implementazion di una transizione in una rete
+ */
 public class Transizione extends ElementoSemplice {
 	
+	/**
+	 * 
+	 * @param nome scelto dal configuratore al momento della creazione
+	 */
 	public Transizione(String nome) {
 		setName(nome);
 	}
 
+	/**
+	 * @return proprietà principali che descrivono una transizione in una rete
+	 */
+	@Override
 	public String getProperties(){
 		return this.getName();
 	}
