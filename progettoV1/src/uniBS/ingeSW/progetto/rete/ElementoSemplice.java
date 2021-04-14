6cef6834ccf6af9f3b1841ae89746ad780e96de6
edoofra <edoardo.fra.ef@gmail.com>
2021-04-14 21:00:05 +0200

    Update ElementoSemplice.java

--- progettoV1/src/uniBS/ingeSW/progetto/rete/ElementoSemplice.java
+++ progettoV1/src/uniBS/ingeSW/progetto/rete/ElementoSemplice.java
@@ -11,7 +11,7 @@ public abstract class ElementoSemplice {
 	/**
 	 * nome dell'elemento della rete
 	 */
-	public String nome;
+	private String nome;
 
 	public String getName(){
 		return this.nome;
