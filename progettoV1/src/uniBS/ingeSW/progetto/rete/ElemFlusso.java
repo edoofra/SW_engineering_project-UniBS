5cbb0ffad940a4e0cc26a4af0d087fe76aee316b
edoofra <edoardo.fra.ef@gmail.com>
2021-04-16 19:31:50 +0200

    Update ElemFlusso.java

--- progettoV1/src/uniBS/ingeSW/progetto/rete/ElemFlusso.java
+++ progettoV1/src/uniBS/ingeSW/progetto/rete/ElemFlusso.java
@@ -34,7 +34,7 @@ public class ElemFlusso {
 	 * @param flusso2
 	 */
 	public boolean equal(ElemFlusso flusso1, ElemFlusso flusso2) {
-	    return ((flusso1.getElem1().equals(flusso2.getElem1()) && 
-		    (flusso1.getElem2().equals(flusso2.getElem2()))));
+	    return ((flusso1.getElem1().equalControl(flusso2.getElem1()) && 
+		    (flusso1.getElem2().equalControl(flusso2.getElem2()))));
 	}	
 }
