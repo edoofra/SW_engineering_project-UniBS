ffededcc469ce254954b5f26606ab7249241c2f3
edoofra <edoardo.fra.ef@gmail.com>
2021-04-16 19:34:10 +0200

    Update Transizione.java

--- progettoV1/src/uniBS/ingeSW/progetto/rete/Transizione.java
+++ progettoV1/src/uniBS/ingeSW/progetto/rete/Transizione.java
@@ -8,7 +8,6 @@ package uniBS.ingeSW.progetto.rete;
 public class Transizione extends ElementoSemplice {
 	
 	/**
-	 * 
 	 * @param nome scelto dal configuratore al momento della creazione
 	 */
 	public Transizione(String nome) {
