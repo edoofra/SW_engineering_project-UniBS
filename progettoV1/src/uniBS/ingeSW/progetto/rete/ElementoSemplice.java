4e34c5d8a17a580762c8dedd2c6bd87ea58e252e
edoofra <edoardo.fra.ef@gmail.com>
2021-05-07 12:20:46 +0200

    javadoc

--- progettoV1/src/uniBS/ingeSW/progetto/rete/ElementoSemplice.java
+++ progettoV1/src/uniBS/ingeSW/progetto/rete/ElementoSemplice.java
@@ -1,10 +1,13 @@
 package uniBS.ingeSW.progetto.rete;
 
 /**
+ * Classe per la gestione ad un livello più alto di posti e transizioni. 
+ * Un elemento semplice è un' <em> astrazione </em> di un posto o di una transizione e 
+ * descrive un elemento di una rete caratterizzato da un nome.
  * @author Lorenzo Bargnani
  * @author Camilla Bonomini
  * @author Edoardo Fratus
- * Classe per la gestione ad un livello più alto di posti e transizioni
+ * @version 1.0
  */
 public class ElementoSemplice {
 	
@@ -18,16 +21,20 @@ public class ElementoSemplice {
 		this.nome = name;
 	}
 
-	//due elementi semplici con lo stesso nome sono considerati uguali
+	/**
+	 * Metodo per controllare se due elementi semplici coincidono.
+	 * Due elementi semplici <em> con lo stesso nome </em> sono considerati uguali.
+	 * @return boolean che indica se i loro nomi sono uguali.
+	 */
 	public boolean equalControl(ElementoSemplice toCheck){
 	    assert toCheck != null : "ElementoSemplice = null"; //precondizione 
 		return (this.nome.equalsIgnoreCase(toCheck.getName()));
 	}
 	
 	/**
-	 * Metodo per ottenere le proprietà principali di ogni elemento 
-	 * Viene fatto override in ogni elemento che eredita da questa classe 
-	 * @return nome dell'elemento (attributo base di ogni elemento della rete)
+	 * Metodo per ottenere le proprietà principali di ogni elemento di una rete.
+	 * In questo caso viene considerato solo il nome come elemento caratterizzante.
+	 * @return nome dell'elemento.
 	 */
 	public String getProperties() {
 		return this.nome;
