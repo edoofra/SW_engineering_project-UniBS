4aa56a9611622d5087673212a628d0f9b55bdac3
edoofra <edoardo.fra.ef@gmail.com>
2021-05-07 12:28:04 +0200

    javadoc

--- progettoV2/src/uniBS/ingeSW/progettoV2/logica/rete/Transizione.java
+++ progettoV2/src/uniBS/ingeSW/progettoV2/logica/rete/Transizione.java
@@ -1,20 +1,24 @@
 package uniBS.ingeSW.progettoV2.logica.rete;
 
 /**
- * @author Edoardo Fratus
+ * Classe che descrive l'implementazione di una transizione all'interno di una rete.
+ * Transizione è vista come un' <em> estensione di un elemento semplice </em>.
  * @author Camilla Bonomini
- * @author Lorenzo Bargnani
- * Classe per l'implementazione di una transizione in una rete
+ * @author Edoardo Fratus
+ * @author Lorenzo Bargnani 
+ * @version 1.0
  */
 public class Transizione extends ElementoSemplice {
 	
 	/**
-	 *
+	 * Prefisso che viene aggiunto al nome di ogni transizone per poterla identificare meglio.
 	 */
 	private static final String PREFISSO_TRANSIZIONE = "T:";
 
 	/**
-	 * @param nome scelto dal configuratore al momento della creazione
+	 * Metodo che assegna un nome alla transizione al momento della creazione.
+	 * Viene aggiunto un prefisso al nome.
+	 * @param nome nome scelto dal configuratore al momento della creazione
 	 */
 	public Transizione(String nome) {
 		setName(PREFISSO_TRANSIZIONE + nome);
