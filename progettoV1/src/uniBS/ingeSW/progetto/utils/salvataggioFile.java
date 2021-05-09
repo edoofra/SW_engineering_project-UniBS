a55974d803a36b2c94b9ddfd448900066006228e
edoofra <edoardo.fra.ef@gmail.com>
2021-05-09 16:32:33 +0200

    Update salvataggioFile.java

--- progettoV1/src/uniBS/ingeSW/progetto/utils/salvataggioFile.java
+++ progettoV1/src/uniBS/ingeSW/progetto/utils/salvataggioFile.java
@@ -5,13 +5,21 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+/**
+ * Classe per il salvataggio e il recuperso da file di stringhe.
+ * @author Lorenzo Bargnani
+ * @author Camilla Bonomini
+ * @author Edoardo Fratus
+ * @version 1.0 
+ */
 public class salvataggioFile {
     
-    /**
-     *
-     */
     private static final String PATH = "ListaRetiConfiguratore.json";
 
+    /**
+     * Metodo per il salvataggio di una stringa su file.
+     * @param daSalvare Stringa da salvare.
+     */
     public static void salvaGestoreReti(String daSalvare){
         assert daSalvare != null : "daSalvare = null"; //precondizione
         try {
@@ -23,6 +31,10 @@ public class salvataggioFile {
         }
     }
 
+    /**
+     * Metodo per la lettura da file di una stringa.
+     * @return stringa letta da file.
+     */
     public static String leggiGestoreRetiDaFile(){
         try {
             String gestoreRetiJSON = new String(Files.readAllBytes(Paths.get(PATH)));
