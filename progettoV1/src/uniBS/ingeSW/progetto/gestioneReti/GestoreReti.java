9805de5ea4c6ef5aef89fdbbfeb61da09b461f0e
edoofra <edoardo.fra.ef@gmail.com>
2021-05-09 16:16:00 +0200

    Update GestoreReti.java

--- progettoV1/src/uniBS/ingeSW/progetto/gestioneReti/GestoreReti.java
+++ progettoV1/src/uniBS/ingeSW/progetto/gestioneReti/GestoreReti.java
@@ -3,11 +3,22 @@ package uniBS.ingeSW.progetto.gestioneReti;
 import java.util.HashMap;
 import uniBS.ingeSW.progetto.rete.Rete;
 
-//classe per la gestione di tutte le reti salvate dal configuratore
+/**
+ * Classe per la gestione delle reti da salvare in modo persistente.
+ * Possono essere inserite solamente reti corrette.
+ * Ogni rete è salvata tramite un nome che deve essere univoco.
+ * @author Edoardo Fratus
+ * @author Lorenzo Bargnani
+ * @author Camilla Bonomini
+ * @version 1.0
+ */
 public class GestoreReti {
 
 	private HashMap<String, Rete> listaRetiConfiguratore;
 
+	/**
+	 * Metodo per inizializzare la lista delle reti da salvare.
+	 */
 	public GestoreReti() {
 		this.listaRetiConfiguratore = new HashMap<String, Rete>();
 	}
@@ -16,16 +27,17 @@ public class GestoreReti {
 		return listaRetiConfiguratore;
 	}
 
-	// ritorna la lista delle chiavi come Array ( piu' comodo da gestire )
 	public String[] getKeyLIst() {
 		return (String[]) listaRetiConfiguratore.keySet().toArray(new String[0]);
 	}
 
-	// aggiunge una rete alla map con controlli
-	// ritorna codice errore 1 se esiste gia' nome
-	// ritorna codice errore 2 se esiste gia' rete
-	// gestione codice errori andra' fatta in interazione con utente
-
+	/**
+	 * Metodo che aggiunge una rete alla lista delle reti da salvare.
+	 * Una rete può essere aggiunta solamente se ha un nome diverso da tutti quelli salvati.
+	 * @param name nome della nuova rete da aggiungere
+	 * @param toAdd rete da aggungere 
+	 * @return boolean che indica se l'aggiunta è andata a buon fine o meno.
+	 */
 	public boolean addRete(String name, Rete toAdd) {
 		assert name != null : "name = null"; //precondizione
 		assert toAdd !=null : "toAdd = null"; //precondizione
@@ -38,10 +50,14 @@ public class GestoreReti {
 			assert nextAdd == beforeAdd +1 : "size error"; //postcondizione
 			return true;
 		}
-
 	}
 
-	
+	/**
+	 * Metodo che controlla se due reti sono uguali.
+	 * @param rete1 prima rete da comparare
+	 * @param rete2 seconda rete da comparare.
+	 * @return boolean che indicano se sono uguali.
+	 */
 	public boolean isEqual(Rete rete1, Rete rete2) {
 		assert rete1 != null : "rete1 = null"; //precondizione
 		assert rete2 != null : "rete2 = null"; //precondizione
