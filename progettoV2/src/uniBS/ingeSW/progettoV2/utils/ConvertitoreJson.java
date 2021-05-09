5ec1756cceac50a4e1a1b3f0dff358e3b2292d57
edoofra <edoardo.fra.ef@gmail.com>
2021-05-09 16:29:22 +0200

    Update ConvertitoreJson.java

--- progettoV2/src/uniBS/ingeSW/progettoV2/utils/ConvertitoreJson.java
+++ progettoV2/src/uniBS/ingeSW/progettoV2/utils/ConvertitoreJson.java
@@ -3,15 +3,31 @@ package uniBS.ingeSW.progettoV2.utils;
 import com.google.gson.Gson;
 import uniBS.ingeSW.progettoV2.logica.gestioneReti.*;
 
-
+/** 
+ * Classe per la conversione di un oggetto GestoreReti da/in formato JSON.
+ * @author Edoardo Fratus
+ * @author Camilla Bonomini
+ * @author Lorenzo Bargnani
+ * @version 1.0
+ */
 public class ConvertitoreJson {
     
+    /**
+     * Metodo che converte un oggetto GestoreReti in una stringa formattata in JSON.
+     * @param oggetto oggetto da convertire.
+     * @return stringa formattata in JSON contenente la descrizione di tutte le reti contenute nell'oggetto GestoreReti.
+     */
     public static String daOggettoAJson(GestoreReti oggetto){
         assert oggetto != null: "oggetto = null"; //precondizione
         Gson convertitore = new Gson();
         return convertitore.toJson(oggetto);
     }
 
+    /**
+     * Metodo che converte una stringa formattata in JSON in un oggetto GestoreReti.
+     * @param json stringa da convertire.
+     * @return oggetto GestoreReti creato a partire da stringa JSON.
+     */
     public static GestoreReti daJsonAOggettoHashSet(String json){
         assert json != null: "json = null"; //precondizione
         Gson convertitore = new Gson();
