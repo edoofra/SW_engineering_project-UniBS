d0fdc2a6939ff514934433ae131e98640cd4abca
edoofra <edoardo.fra.ef@gmail.com>
2021-05-17 22:07:40 +0200

    Create MarcaturaView.java

---
+++ progettoV2/src/uniBS/ingeSW/progettoV2/view/MarcaturaView.java
@@ -0,0 +1,28 @@
+package uniBS.ingeSW.progettoV2.view;
+
+import uniBS.ingeSW.progettoV2.logica.rete.Posto;
+import uniBS.ingeSW.progettoV2.logica.retePetri.MarcaturaPN;
+
+public class MarcaturaView {
+    
+    MarcaturaPN marcatura;
+
+    public MarcaturaView(MarcaturaPN daPresentare){
+        this.marcatura = daPresentare;
+    }
+
+    public String toString(){
+        StringBuilder builder = new StringBuilder("Marcatura: \n { ");
+        for(Posto eachPosto : marcatura.getMarcatura().keySet().toArray(new Posto[0])){
+            builder.append(eachPosto.toString() + ":");
+            builder.append(marcatura.getMarcatura().get(eachPosto) + ", ");
+        }
+        int lenght = builder.length();
+        builder.delete(lenght-1, lenght);
+        builder.append(" }");
+        return builder.toString();
+
+    }
+
+
+}
