5a253ac6a6af4c95ac546abce3f6783938d3434f
edoofra <edoardo.fra.ef@gmail.com>
2021-05-26 11:17:28 +0200

    Update ConvertitoreJson.java

--- progettoV2/src/uniBS/ingeSW/progettoV2/utils/ConvertitoreJson.java
+++ progettoV2/src/uniBS/ingeSW/progettoV2/utils/ConvertitoreJson.java
@@ -33,4 +33,26 @@ public class ConvertitoreJson {
         Gson convertitore = new Gson();
         return convertitore.fromJson(json, GestoreReti.class);
     }
+
+     /**
+     * Metodo che converte un oggetto GestoreReti in una stringa formattata in JSON.
+     * @param oggetto oggetto da convertire.
+     * @return stringa formattata in JSON contenente la descrizione di tutte le reti contenute nell'oggetto GestoreReti.
+     */
+    public static String daOggettoAJson(GestoreRetiPetri oggetto){
+        assert oggetto != null: "oggetto = null"; //precondizione
+        Gson convertitore = new Gson();
+        return convertitore.toJson(oggetto);
+    }
+
+     /**
+     * Metodo che converte una stringa formattata in JSON in un oggetto GestoreReti.
+     * @param json stringa da convertire.
+     * @return oggetto GestoreReti creato a partire da stringa JSON.
+     */
+    public static GestoreRetiPetri daJsonAOggettoPetriHashSet(String json){
+        assert json != null: "json = null"; //precondizione
+        Gson convertitore = new Gson();
+        return convertitore.fromJson(json, GestoreRetiPetri.class);
+    }
 }
