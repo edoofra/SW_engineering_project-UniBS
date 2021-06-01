12405f40613cf9ddf5ad252526c0dae9c6a2ea89
edoofra <edoardo.fra.ef@gmail.com>
2021-06-01 23:05:18 +0200

    add libraries

--- progettoV2/src/uniBS/ingeSW/progettoV2/utils/ConvertitoreJson.java
+++ progettoV2/src/uniBS/ingeSW/progettoV2/utils/ConvertitoreJson.java
@@ -1,5 +1,8 @@
 package uniBS.ingeSW.progettoV2.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import uniBS.ingeSW.progettoV2.logica.gestioneReti.*;
 
@@ -53,6 +56,15 @@ public class ConvertitoreJson {
     public static GestoreRetiPetri daJsonAOggettoPetriHashSet(String json){
         assert json != null: "json = null"; //precondizione
         Gson convertitore = new Gson();
+//        try {
+//	    GestoreRetiPetri message= new ObjectMapper().readValue(json, GestoreRetiPetri.class);
+//	} catch (JsonMappingException e) {
+//	    // TODO Auto-generated catch block
+//	    e.printStackTrace();
+//	} catch (JsonProcessingException e) {
+//	    // TODO Auto-generated catch block
+//	    e.printStackTrace();
+//	}
         return convertitore.fromJson(json, GestoreRetiPetri.class);
     }
 }
