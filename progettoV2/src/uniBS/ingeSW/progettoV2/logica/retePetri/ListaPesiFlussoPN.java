1502256aca8d492021a3dd744d8e7d4d3332111a
Ekastos <77198583+Ekastos@users.noreply.github.com>
2021-06-20 16:51:51 +0200

    Update ListaPesiFlussoPN.java

--- progettoV2/src/uniBS/ingeSW/progettoV2/logica/retePetri/ListaPesiFlussoPN.java
+++ progettoV2/src/uniBS/ingeSW/progettoV2/logica/retePetri/ListaPesiFlussoPN.java
@@ -21,7 +21,11 @@ private LinkedHashMap<ElemFlusso, Integer> listaPesi;
 	public HashMap<ElemFlusso, Integer> getListaPesi() {
 		return listaPesi;
 	}
-    
+    /**
+	* Metodo che imposta il peso di un elemento di flusso (il valore è preimpostato ad 1 alla creazione della rete di petri) 
+	* @param nomeElemento1 primo elemento, nomeElemento2 secondo elemento, nuovoPeso Peso da impostare
+	* @return boolean che indica se la modifica è andata a buon fine
+	*/
     public boolean impostaPeso(String nomeElemento1, String nomeElemento2, int nuovoPeso) {
     	ElemFlusso[] elemFlussoArray = listaPesi.keySet().toArray(new ElemFlusso[0]);
     	ElemFlusso elemFlussoDaCambiare = Stream.of(elemFlussoArray)
@@ -36,7 +40,10 @@ private LinkedHashMap<ElemFlusso, Integer> listaPesi;
     	return false;	
      }
 
-	 //controlla se due listePesi sono uguali
+	 /** Metodo che controlla se due listePesi sono uguali
+	 * @param toCompare listaPesiFlussoPN da confrontare con la listaPesi su cui è stato chiamato il metodo
+	 * @return boolean che ritorna true se la lista è uguale, false se la ListaPesiFlussoPN è diversa.  
+	 */
 	 public boolean isEqual(ListaPesiFlussoPN toCompare){
 		ElemFlusso[] arrayFlusso1 = this.getListaPesi().keySet().toArray(new ElemFlusso[0]);
 		ElemFlusso[] arrayFlusso2 = toCompare.getListaPesi().keySet().toArray(new ElemFlusso[0]);
