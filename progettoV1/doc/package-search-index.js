a5180ba29d5a42c48ae0d92ede88de7d485eaea3
edoofra <edoardo.fra.ef@gmail.com>
2021-07-19 15:02:20 +0200

    create doc

---
+++ progettoV1/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"uniBS.ingeSW.progetto.gestioneReti"},{"l":"uniBS.ingeSW.progetto.rete"},{"l":"uniBS.ingeSW.progetto.utils"}]
