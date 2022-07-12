bd524a8c009d5424f5623218a8fdfdeb0ee01c94
Edoardo Fratus <edoardo.fra.ef@gmail.com>
2022-07-12 21:06:26 +0200

    Delete progettoV1/doc directory

--- progettoV1/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"uniBS.ingeSW.progetto.gestioneReti"},{"l":"uniBS.ingeSW.progetto.rete"},{"l":"uniBS.ingeSW.progetto.utils"}]
